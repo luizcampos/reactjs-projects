bc894c27cfb4d416877934eeb889ff45ecc55dd6
luizcampos <felipe_luiz96@yahoo.com>
2022-08-15 07:00:27 -0300

    estilização do header

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post author="Luiz Fellipe" content="Olá, prazer!" />
       <Post author="Beyoncé Knowles" content="Heey, baby!" />
     </div>
---
+++ 01-fundamentos-react/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+import igniteLogo from '../assets/ignite-logo.svg'; /* retorna o caminho */
+
+export function Header(){
+    return(
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logotipo do Ignite"></img>
+            Ignite Feed
+        </header>
+    );
+}
